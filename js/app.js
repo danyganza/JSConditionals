653bacf95a5176753ec55b02b8612c6f925703a8
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-03-14 20:13:47 -0600

    added

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+let item_one_value=90;
+if(user_points>=75){
+    console.log ( "you did it . great job" )
+} else if (user_points=50){
+    console.log("keep going, you are almost at 75")
+} else {
+    console.log("keep going,get more points")
+}
+
+
