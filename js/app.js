915e32b526e7af2dce3860fdc72ba99dc22bb7a2
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-04-21 00:12:45 -0600

    corrected syntax

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 
-let item_one_value=90;
+let user_points=75;
+let is_member= true
+let user_name= `Alex`
+
 if(user_points>=75){
     console.log ( "you did it . great job" )
 } else if (user_points=50){
@@ -7,5 +10,21 @@ if(user_points>=75){
 } else {
     console.log("keep going,get more points")
 }
+
+
+
+if(is_member===true && user_points>= true){
+    console.log ("talented member")  
+}
+
+if(is_member>=false || user_points<10){
+    console.log (" you are taelneted and member, must leave")  
+}
+ if(user_name === "Alex"){
+    console.log ("you are a very special user")
+ } else {
+ console.log("you are a normal user")
+ };
+
 
 
